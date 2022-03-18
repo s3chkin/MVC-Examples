73b349f38e5099c3bcc55e030c87cdb7ea2b7354
s3chkin <seckins191@gmail.com>
2022-03-18 13:16:56 +0200

    added new examples

---
+++ ConsoleMVC/ArrayExampleCross/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExampleCross
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firtsDay = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] lastDay = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine("\n =====================");
+
+            for (int i = 0; i < firtsDay.Length; i++)
+            {
+
+                Console.Write(firtsDay[i] + " ");
+                Array.Sort(firtsDay);
+            }
+            
+            for (int i = 0; i < lastDay.Length; i++)
+            {
+                Console.Write(lastDay[i] + " ");
+                Array.Sort(lastDay);
+
+            }
+            //Console.WriteLine("\n");
+            //var rslt = firtsDay.ToString() + lastDay.ToString();
+            //Array.Sort(rslt.ToArray());
+
+            //Console.WriteLine(rslt);
+
+
+        }
+    }
+}
---
+++ ConsoleMVC/ArrayExamples/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i] + " ");
+                //if (nums[i] == nums[i])
+                //{
+                //    Console.WriteLine("Има еднакви!");
+                //}
+                //else if (nums[i] != nums[i])
+                //{
+                //    Console.WriteLine("“No occurrences were found!”.");
+                //}
+                if (nums[i] == nums[i] )
+                {
+                    Console.Write("  Има еднакви! ");
+
+                }
+            }
+
+            //if (nums[0] == nums[i] || nums[1] == nums[1] || nums[2] == nums[2] || nums[3] == nums[3]
+            //    || nums[3] == nums[3] || nums[4] == nums[4] || nums[5] == nums[5]
+            //    || nums[6] == nums[6])
+            //{
+            //    Console.WriteLine("Има еднакви!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nyama еднакви!");
+            //}
+
+
+        }
+    }
+}
